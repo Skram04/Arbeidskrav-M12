bbbfb5b71edc71bdba1c2a87b521ebd661763795
Skram04 <krskramstad2@gmail.com>
2026-03-23 19:12:37 +0100

    first commit

---
+++ Oppgave 1.py
@@ -0,0 +1,11 @@
+def f(x): #Definer en funksjon
+    return (x+7)/(2+x**2) #Funksjonen returnerer regnestykket med x som en variabel
+
+x = 0 #Definerer variabelen x som 0
+h = 0.001 #Definerer variabelen h som 0.001
+
+while f(x) <= f(x+h):   #While loop som kjører enn så lenge f(x) er mindre eller lik f(x+h)
+                        #Det gjør denne funksjonen til derivasjon av f(x)
+    x=x+h               #Øker x med h som er veldig lite, derav derivasjon
+
+print (x)               #Skriver ut variabelen x
---
+++ Oppgave 1d og e.py
@@ -0,0 +1,15 @@
+def f(x): 
+    return (v0*x-0.5*g*x**2)
+
+def f(t):
+    return ()
+
+v0 = 150
+g = 9.81
+x = 0 
+h = 0.001
+
+while f(x) <= f(x+h):   
+    x=x+h               
+
+print (x)
