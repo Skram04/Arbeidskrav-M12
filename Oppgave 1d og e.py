4ba6508d8ef4e8fbddc3402fbe56e2a7dfed91cc
Skram04 <krskramstad2@gmail.com>
2026-03-23 20:22:12 +0100

    DRIT

--- Oppgave 1d og e.py
+++ Oppgave 1d og e.py
@@ -1,15 +1,18 @@
-def f(x): 
-    return (v0*x-0.5*g*x**2)
+import math as m
 
-def f(t):
-    return ()
+def f(x):
+    return (-v0 / g)
 
+def y(x): 
+    return ((v0 + m.sqrt(v0**2 * (-4) * 4.905 * (-15.3)) / 2 * 4.905))
+
+x = 0
 v0 = 150
-g = 9.81
-x = 0 
-h = 0.001
+g = -9.81 
+a = 1 / 2 * -g
+b = v0
+c = f(x)
 
-while f(x) <= f(x+h):   
-    x=x+h               
 
-print (x)
+print (f(x))
+print (y(0))
