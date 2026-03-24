df5c3db1a6c16314309d32e7e73d70c568e16a5e
Skram04 <krskramstad2@gmail.com>
2026-03-24 17:16:13 +0100

    Oppgave d&e på oppgave 1 fungerer.

--- Oppgave_1d&e.py
+++ Oppgave_1d&e.py
@@ -1,18 +1,12 @@
-from math import sqrt
-
-x = 0
 v = 0
 v0 = 150
-g = -9.81 
-a = 1 / 2 * -g
-b = v0
+g = -9.81
 
 def f():
-    return (v-v0)
+    return v-v0 / g
 
-def y(): 
-    pluss = (150 + sqrt(150**2 * (-4) * 4.905 * (-15.3))) / (2 * 4.905)
-    minus = v0 - sqrt(v0**2 * (-4) * 4.905 * (-15.3)) / (2 * 4.905)
+def y():
+    return (v + v0) / 2 * f()
 
-print (f(), "funksjon f")
-print (y(), "funksjon y")
+print ("Tid = ", f())
+print ("Strekning = ", y())
