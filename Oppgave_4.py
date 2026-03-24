5690ab7d94bab14c02561b2622516efb3e3ca112
Skram04 <krskramstad2@gmail.com>
2026-03-24 19:42:39 +0100

    La til gitignore og endret litt i arbeidskrav 4

--- Oppgave_4.py
+++ Oppgave_4.py
@@ -1,8 +1,29 @@
+from math import sqrt
+
 fart = [73,69,81,83,64,59,70]
 dist = 0
 
+a = 0
+b = 90
+n = 10000
+
+dx = (b - a) / n
+print ("delta x er", dx)
+
 for f in fart:
     dist += f * 0.25        #Et kvarter = 0.25 timer
-dist += (-0.5 * fart[0] + 0.5 * fart[-1]) * 0.25
+#dist += (-0.5 * fart[0] + 0.5 * fart[-1]) * 0.25
 
 print (dist)
+
+def f(x):
+    return 3*sqrt(x)
+
+s = 0
+"""
+for i in range (n):
+    s += f(a + i * dx) *dx
+s += (-0.5*f(a) + 0.5 f(b) * dx)
+
+print ("Areal er", s)
+"""
