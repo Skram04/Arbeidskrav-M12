ae9f4db5d3cb28ae199dfac756181f3b586b76fc
Skram04 <krskramstad2@gmail.com>
2026-03-25 14:50:27 +0100

    La til kommentarer

--- Oppgave_1d&e.py
+++ Oppgave_1d&e.py
@@ -4,9 +4,9 @@ h = 0.001
 x = 0
 
 def f(x):
-    return v0 * x + 0.5 * g * x**2
+    return v0 * x + 0.5 * g * x**2          #Formel som finner strekningen med konstant akselerasjon
 
-while f(x) <= f(x+h):
+while f(x) <= f(x+h):                       #Finner toppunktet til funksjonen
     x += h
 
-print ("Tiden når kulen er på det høyeste punktet er", x)
+print ("Tiden når kulen er på det høyeste punktet er", x) 
