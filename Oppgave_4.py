989f033aae362e6dc1869e56ffeb0afaf150b293
Skram04 <krskramstad2@gmail.com>
2026-03-25 08:35:19 +0100

    Fjernet print linjer i oppgave 4.

--- Oppgave_4.py
+++ Oppgave_4.py
@@ -2,10 +2,8 @@ fart = [73,69,81,83,64,59,70]
 dist = 0
 
 for f in fart:
-    print ("f er = ", f)
-    print ("fart index er = ", fart.index(f))
     dist += (fart[fart.index(f)]) * 0.25
-    print("distanse er = ", dist)
+    
 dist += (-1/2 * fart[0] - 1/2 * fart[-1] ) * 0.25               #Fjerner halvparten av første og siste punkt
 
 print ("distanse = ", dist)
