3b2087114aa10d5bee25cd4160a0de8a12d6c449
Skram04 <krskramstad2@gmail.com>
2026-03-25 10:40:21 +0100

    Fjernet print statement

--- Oppgave_4.py
+++ Oppgave_4.py
@@ -2,7 +2,6 @@ fart = [73,69,81,83,64,59,70]
 dist = 0
 
 for f in fart:
-    print (f)
     dist += f * 0.25
 
 dist += (-1/2 * fart[0] - 1/2 * fart[-1] ) * 0.25               #Fjerner halvparten av første og siste punkt
