181e75ecc7b418d7ec0f849603d75a6e2cbb2c63
Skram04 <krskramstad2@gmail.com>
2026-03-25 11:28:08 +0100

    Fjernet parametere

--- Oppgave_1d&e.py
+++ Oppgave_1d&e.py
@@ -1,8 +1,12 @@
-def t(v,v0,g):                                  #Funksjon som finner tiden til toppunktet
+v = 0       #m/s   
+v0 = 150     #m/s**2
+g = -9.81   #m/s**2
+
+def t():                                  #Funksjon som finner tiden til toppunktet
     return v-v0 / g
 
-def s(v,v0,g):
+def s():
     return (v + v0) / 2 * t(v,v0,g)             #Funksjon som finner hvor langt opp kula går
 
-print ("Tid = ", t(0, 150, -9.81))
-print ("Strekning = ", s(0, 150, -9.81))        
+print ("Tid = ", t())
+print ("Strekning = ", s())        
